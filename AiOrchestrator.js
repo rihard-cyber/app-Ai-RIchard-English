8e2a806901b703950ad7bce5f5f3052fbd5ee939
Richard_cyber <128294864+rihard-cyber@users.noreply.github.com>
2026-05-05 15:09:46 +0700

    Perbaikan APP

--- AiOrchestrator.js
+++
@@ -1,9 +0,0 @@
-import { generateChatResponse } from './ChatService';
-import { translateText } from './TranslationService';
-import { speakText } from './VoiceService';
-
-export const AiOrchestrator = {
-    chat: generateChatResponse,
-    translate: translateText,
-    speak: speakText
-};
